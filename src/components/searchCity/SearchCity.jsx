92520d5a215f54ea86b707cf55ca24b1fe9d12c3
ibnsan <61366559+ibnsan@users.noreply.github.com>
2021-11-26 15:48:22 +0200

    Search autocomplete
    Used google maps AutocompleteService. I connected the script to index and at the same time removed the unnecessary there.

--- src/components/searchCity/SearchCity.jsx
+++ src/components/searchCity/SearchCity.jsx
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
 import AppInupt from "../ui/Input/AppInupt";
 import {useTranslation} from "react-i18next";
-import style from './SearchCity.module.css'
 import AppButton from "../ui/Button/AppButton";
+import style from './SearchCity.module.css'
+import Dropdown from "../ui/Dropdown/Dropdown";
+import globalStyle from "../../styles/global.module.css";
 
 const SearchCity = () => {
+    const [userQuery, setUserQuery] = useState("");
+    const [citiesList, setCitiesList] = useState([])
     const {t} = useTranslation()
+    const [statusShowDropdown, setStatusShowDropdown] = useState(false)
+
+    useEffect(() => {
+        if (userQuery) {
+            let autoComplete;
+            let sessionToken;
+
+            autoComplete = new window.google.maps.places.AutocompleteService();
+            sessionToken = new window.google.maps.places.AutocompleteSessionToken();
+
+            autoComplete.getPlacePredictions({
+                input: userQuery,
+                sessionToken: sessionToken,
+                types: ["(cities)"],
+            }).then(e => {
+                console.log(e)
+                setCitiesList(e.predictions)
+                if (e.predictions.length > 0) {
+                    setStatusShowDropdown(true)
+                } else {
+                    setStatusShowDropdown(false)
+                }
+            }).catch(err => (console.log(err)))
+        }
+    }, [userQuery]);
+
     return (
         <div className={style.parentBlock}>
             <div className={style.dataBlock}>
-                <AppInupt type="text" placeholder={t('searchInputPlaceholder')} />
-                <AppButton>{t('addButtonInSearchPanel')}</AppButton>
+                <div className={style.inputBlock}>
+                    <AppInupt onChange={event => setUserQuery(event.target.value)}
+                              value={userQuery}
+                              type="text"
+                              placeholder={t('searchInputPlaceholder')}/>
+                    <Dropdown status={statusShowDropdown} setStatus={setStatusShowDropdown}>
+                        <select className={style.selectBlock} size="4">
+                            {citiesList.length > 0
+                                ?
+                                citiesList.map((item) =>
+                                <option value={item.structured_formatting.main_text}
+                                        className={globalStyle.dropdownContent} key={item.place_id}>
+                                    {item.description}
+                                </option>)
+                                    :
+                                <option>no data</option>
+                            }
+                        </select>
+                    </Dropdown>
+                </div>
+                <div className={style.btnBlock}>
+                    <AppButton>{t('addButtonInSearchPanel')}</AppButton>
+                </div>
             </div>
         </div>
+
     );
-};
+}
 
 export default SearchCity;
